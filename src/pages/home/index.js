d22772959d2ca8d68db02c164ea6483fffa3cbf7
JonasOliveira <jonaspjonas@gmail.com>
2021-12-30 11:09:06 -0300

    update

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,7 +3,7 @@ import './style.css';
 function Home() {
     return (
         <div className="home-container">
-            <h1>Cartao de vacinaçao</h1>
+            <h1>Cartão de vacinação</h1>
             <h2>Usuarios</h2>
             
                 <div className='lista-usuarios'>
@@ -15,14 +15,22 @@ function Home() {
                     <div>
                         <ul>Data de nascimento</ul>
                         <li>15/05/2000</li>
+                        <li>22/08/1978</li>
                     </div>
                     <div>
-                        <ul>Cartao SUS</ul>
+                        <ul>Cartão SUS</ul>
                         <li>700235698745125</li>
+                        <li>999998888888554</li>
                     </div>
                     <div>
                         <ul>Telefone</ul>
                         <li>(91)99564-5781</li>
+                        <li>(91)99564-5781</li>
+                    </div>
+                    <div>
+                        <ul>Opções</ul>
+                        <li><img src="" alt="Vacinar"/><img src="" alt="Editar"/><img src="" alt="Excluir"/></li>
+                        <li><img src="" alt="Vacinar"/><img src="" alt="Editar"/><img src="" alt="Excluir"/></li>
                     </div>
 
             </div>
